c4356982d5d8f1753e3cb77891bb5f572a69995c
linmudaye <52953782+linmudaye@users.noreply.github.com>
2021-07-08 23:50:41 +0800

    Update jd_summer_movement.js

--- jd_summer_movement.js
+++ jd_summer_movement.js
@@ -18,7 +18,15 @@ const { R_OK } = require('fs').constants;
 const vm = require('vm');
 let smashUtils;
 
-const summer_movement_joinjoinjoinhui = $.isNode() ? (process.env.summer_movement_joinjoinjoinhui ? process.env.summer_movement_joinjoinjoinhui : true) : ($.getdata("summer_movement_joinjoinjoinhui") ? $.getdata("summer_movement_joinjoinjoinhui") : false);;//是否入会  true 入会，false 不入会
+let summer_movement_joinjoinjoinhui = true;//是否入会  true 入会，false 不入会
+if ($.isNode() && process.env.summer_movement_joinjoinjoinhui) {
+  summer_movement_joinjoinjoinhui = process.env.summer_movement_joinjoinjoinhui;
+}
+
+let ShHelpFlag = false;//是否SH助力  true 助力，false 不助力
+if ($.isNode() && process.env.ShHelpFlag) {
+  ShHelpFlag = process.env.ShHelpFlag;
+}
 
 const ShHelpFlag = $.isNode() ? (process.env.summer_movement_ShHelpFlag ? process.env.summer_movement_ShHelpFlag : true) : ($.getdata("summer_movement_ShHelpFlag") ? $.getdata("summer_movement_ShHelpFlag") : true);;//是否SH助力  true 助力，false 不助力
 
