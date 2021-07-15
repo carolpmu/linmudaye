3077442e4967192466a6d0ef231c19cdc00daac7
linmudaye <52953782+linmudaye@users.noreply.github.com>
2021-07-15 11:14:38 +0800

    Update jd_joy_steal.js

--- jd_joy_steal.js
+++ jd_joy_steal.js
@@ -23,7 +23,7 @@ cron "10 0-21/3 * * *" script-path=jd_joy_steal.js,tag=宠汪汪偷好友积分�
 宠汪汪偷好友积分与狗粮 = type=cron,script-path=jd_joy_steal.js, cronexpr="10 0-21/3 * * *", timeout=3600, enable=true
 */
 const $ = new Env('宠汪汪偷好友积分与狗粮');
-const validator = require('./utils/JDJRValidator_Pure');
+const validator = require('./JDJRValidator_Pure');
 $.get = validator.injectToRequest($.get.bind($));
 $.post = validator.injectToRequest($.post.bind($));
 const notify = $.isNode() ? require('./sendNotify') : '';
