d99fdca545b6814cc80b25131918cc3e57347951
linmudaye <52953782+linmudaye@users.noreply.github.com>
2021-07-23 15:19:40 +0800

    Update jd_summer_movement_map.js

--- jd_summer_movement_map.js
+++ jd_summer_movement_map.js
@@ -1,7 +1,7 @@
 /**
  *  燃动夏季领店铺任务
  *  蚊子腿金币
- *  cron 7 10,20 * * *
+cron 7 10,20 * * * jd_summer_movement_map.js
  * */
 const $ = new Env('燃动夏季领店铺任务');
 const notify = $.isNode() ? require('./sendNotify') : '';
