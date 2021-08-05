30e25aee0f6dc482bbc4a68754d8969085ca74a8
linmudaye <52953782+linmudaye@users.noreply.github.com>
2021-08-05 20:40:02 +0800

    Update jd_joy_reward.js

--- jd_joy_reward.js
+++ jd_joy_reward.js
@@ -20,11 +20,11 @@ cron "59 7,15,23 * * *" script-path=jd_joy_reward.js,tag=宠汪汪积分兑换�
  */
 // prettier-ignore
 const $ = new Env('宠汪汪积分兑换奖品');
-const zooFaker = require('./JDJRValidator_Pure');
+const zooFaker = require('./utils/JDJRValidator_Pure');
 // $.get = zooFaker.injectToRequest2($.get.bind($));
 // $.post = zooFaker.injectToRequest2($.post.bind($));
 let allMessage = '';
-let joyRewardName = 500;//是否兑换京豆，默认0不兑换京豆，其中20为兑换20京豆,500为兑换500京豆，0为不兑换京豆.数量有限先到先得
+let joyRewardName = 0;//是否兑换京豆，默认0不兑换京豆，其中20为兑换20京豆,500为兑换500京豆，0为不兑换京豆.数量有限先到先得
 //Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 const notify = $.isNode() ? require('./sendNotify') : '';
@@ -114,15 +114,15 @@ async function joyReward() {
         // console.log(`当前积分 ${data.coin}\n`);
         // console.log(`宠物等级 ${data.level}\n`);
         let saleInfoId = '', giftValue = '', extInfo = '', leftStock = 0, salePrice = 0;
-        let rewardNum = 500;
+        let rewardNum = 0;
         if ($.isNode() && process.env.JD_JOY_REWARD_NAME) {
-          rewardNum = process.env.JD_JOY_REWARD_NAME * 500;
+          rewardNum = process.env.JD_JOY_REWARD_NAME * 1;
         } else if ($.getdata('joyRewardName')) {
-          if ($.getdata('joyRewardName') * 1 === 500) {
+          if ($.getdata('joyRewardName') * 1 === 1) {
             //兼容之前的BoxJs设置
-            rewardNum = 500;
+            rewardNum = 20;
           } else {
-            rewardNum = $.getdata('joyRewardName') * 500;
+            rewardNum = $.getdata('joyRewardName') * 1;
           }
         } else {
           rewardNum = joyRewardName;
