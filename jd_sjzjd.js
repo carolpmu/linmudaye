daf736814186ab9bb4875bc94b0ba01a7a9fa619
linmudaye <52953782+linmudaye@users.noreply.github.com>
2021-09-01 20:48:27 +0800

    Update jd_sjzjd.js

--- jd_sjzjd.js
+++ jd_sjzjd.js
@@ -2,7 +2,7 @@
 // https://h5.m.jd.com/rn/42yjy8na6pFsq1cx9MJQ5aTgu3kX/index.html
 
 入口：首页-领京豆-升级赚京豆
-21 9 * * * https://raw.githubusercontent.com/linmudaye/linmudaye/main/jd_sjzjd.js, tag=升级赚京豆, enabled=true
+21 9 * * * jd_sjzjd.js, tag=MM领京豆, enabled=true
 
 */
 
