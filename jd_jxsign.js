166aa2f7868525d22bbe1034467570b1936f89c3
linmudaye <52953782+linmudaye@users.noreply.github.com>
2021-09-03 10:41:44 +0800

    Update jd_jxsign.js

--- jd_jxsign.js
+++ jd_jxsign.js
@@ -3,21 +3,17 @@
 cron 20 1,8 * * * jd_jxsign.js
 更新时间：2021-7-31
 活动入口：京喜APP-我的-京喜签到
-
 已支持IOS双京东账号,Node.js支持N个京东账号
 脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
 ============Quantumultx===============
 [task_local]
 #京喜签到
 20 1,8 * * * https://raw.githubusercontent.com/linmudaye/linmudaye/main/jd_jxsign.js, tag=京喜签到, img-url=https://raw.githubusercontent.com/58xinian/icon/master/jxcfd.png, enabled=true
-
 ================Loon==============
 [Script]
 cron "20 1,8 * * *" script-path=https://raw.githubusercontent.com/linmudaye/linmudaye/main/jd_jxsign.js,tag=京喜签到
-
 ===============Surge=================
 京喜签到 = type=cron,cronexp="20 1,8 * * *",wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/linmudaye/linmudaye/main/jd_jxsign.js
-
 ============小火箭=========
 京喜签到 = type=cron,script-path=https://raw.githubusercontent.com/linmudaye/linmudaye/main/jd_jxsign.js, cronexpr="20 1,8 * * *", timeout=3600, enable=true
  */
@@ -66,7 +62,7 @@ if ($.isNode()) {
         continue
       }
       if (i === 0) console.log(`\n正在收集助力码请等待\n`)
-      UA = `jdpingou;iPhone;4.13.0;14.4.2;${randomString()};network/wifi;model/iPhone10,2;appBuild/100609;ADID/00000000-0000-0000-0000-000000000000;supportApplePay/1;hasUPPay/0;pushNoticeIsOpen/1;hasOCPay/0;supportBestPay/0;session/${Math.random * 98 + 1};pap/JA2019_3111789;brand/apple;supportJDSHWK/1;Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148`
+      UA = `jdpingou;iPhone;4.13.0;14.4.2;${randomString(40)};network/wifi;model/iPhone10,2;appBuild/100609;ADID/00000000-0000-0000-0000-000000000000;supportApplePay/1;hasUPPay/0;pushNoticeIsOpen/1;hasOCPay/0;supportBestPay/0;session/${Math.random * 98 + 1};pap/JA2019_3111789;brand/apple;supportJDSHWK/1;Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148`
       await signhb(1)
       await $.wait(500)
       UAInfo[$.UserName] = UA
@@ -191,7 +187,7 @@ function signhb(type = 1) {
               for (let key of Object.keys(signlist)) {
                 let vo = signlist[key]
                 if (vo.istoday === 1) {
-                  if (vo.status === 1 && data.signtask.status === 1) {
+                  if (vo.status === 1 && data.todaysign === 1) {
                     console.log(`今日已签到`)
                     $.canHelp = false
                   } else {
@@ -240,7 +236,7 @@ function helpSignhb(smp = '') {
           for (let key of Object.keys(signlist)) {
             let vo = signlist[key]
             if (vo.istoday === 1) {
-              if (vo.status === 1 && data.signtask.status === 1) {
+              if (vo.status === 1 && data.todaysign === 1) {
                 // console.log(`今日已签到`)
               } else {
                 console.log(`此账号已黑`)
@@ -362,13 +358,14 @@ function taskUrl(functionId, body = '', stk) {
     }
   }
 }
-function randomString() {
-  return Math.random().toString(16).slice(2, 10) +
-    Math.random().toString(16).slice(2, 10) +
-    Math.random().toString(16).slice(2, 10) +
-    Math.random().toString(16).slice(2, 10) +
-    Math.random().toString(16).slice(2, 10)
+function randomString(e) {
+  e = e || 32;
+  let t = "0123456789abcdef", a = t.length, n = "";
+  for (let i = 0; i < e; i++)
+    n += t.charAt(Math.floor(Math.random() * a));
+  return n
 }
+
 
 function TotalBean() {
   return new Promise(async resolve => {
