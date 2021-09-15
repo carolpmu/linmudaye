7e5b1e66d6e9c90ec4bb99837fe53645565c41f5
linmudaye <52953782+linmudaye@users.noreply.github.com>
2021-09-15 21:05:46 +0800

    Update jd_wish.js

--- jd_wish.js
+++ jd_wish.js
@@ -5,17 +5,14 @@
 ===============Quantumultx===============
 [task_local]
 #众筹许愿池
-40 0,2 * * * https://raw.githubusercontent.com/linmudaye/linmudaye/main/jd_wish.js, tag=众筹许愿池, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
-
+40 0,2 * * * jd_wish.js, tag=众筹许愿池, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
 ================Loon==============
 [Script]
-cron "40 0,2 * * *" script-path=https://raw.githubusercontent.com/linmudaye/linmudaye/main/jd_wish.js,tag=众筹许愿池
-
+cron "40 0,2 * * *" script-path=jd_wish.js,tag=众筹许愿池
 ===============Surge=================
-众筹许愿池 = type=cron,cronexp="40 0,2 * * *",wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/linmudaye/linmudaye/main/jd_wish.js
-
+众筹许愿池 = type=cron,cronexp="40 0,2 * * *",wake-system=1,timeout=3600,script-path=jd_wish.js
 ============小火箭=========
-众筹许愿池 = type=cron,script-path=https://raw.githubusercontent.com/linmudaye/linmudaye/main/jd_wish.js, cronexpr="40 0,2 * * *", timeout=3600, enable=true
+众筹许愿池 = type=cron,script-path=jd_wish.js, cronexpr="40 0,2 * * *", timeout=3600, enable=true
  */
 const $ = new Env('众筹许愿池');
 const notify = $.isNode() ? require('./sendNotify') : '';
@@ -25,8 +22,8 @@ let message = '', allMessage = '';
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '';
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
-let appIdArr = ['1E1NXxq0'];
-let appNameArr = ['众筹许愿池'];
+let appIdArr = ['1E1NXxq0', '1ElBTx6o'];
+let appNameArr = ['众筹许愿池', '企有此礼'];
 let appId, appName;
 $.shareCode = [];
 if ($.isNode()) {
