896b6371b214e794fd97e21075cfd4496b0f7824
linmudaye <52953782+linmudaye@users.noreply.github.com>
2021-11-01 09:55:09 +0800

    Update jd_fcwb.js

--- jd_fcwb.js
+++ jd_fcwb.js
@@ -30,9 +30,9 @@ let fcwbinviteCode = "";
     }
     console.log(`\n注意：本脚本暂时只会执行助力，助力后，请手动进活动进行游戏（发财挖宝: 入口,极速版-》我的-》发财挖宝）\n`)
     let res = [];
-    try{res = await getAuthorShareCode('https://raw.githubusercontent.com/lsh26/share_code/main/fcwb.json');}catch (e) {}
+    try{res = await getAuthorShareCode('https://raw.githubusercontent.com/linmudaye/updateTeam/master/fcwb.json');}catch (e) {}
     if(!res){
-        try{res = await getAuthorShareCode('https://cdn.jsdelivr.net/gh/lsh26/share_code@main/fcwb.json');}catch (e) {}
+        try{res = await getAuthorShareCode('https://cdn.jsdelivr.net/gh/linmudaye/updateTeam@master/shareCodes/fcwb.json');}catch (e) {}
         if(!res){res = [];}
     }
     if(res.length > 0){
