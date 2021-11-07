cca092c5e05da8f3ae481da424254cfbe8a6041d
linmudaye <52953782+linmudaye@users.noreply.github.com>
2021-11-07 19:56:01 +0800

    Update jd_speed_sign.js

--- jd_speed_sign.js
+++ jd_speed_sign.js
@@ -106,7 +106,7 @@ async function signInit() {
     $.get(taskUrl('speedSignInit', {
       "activityId": "8a8fabf3cccb417f8e691b6774938bc2",
       "kernelPlatform": "RN",
-      "inviterId":"U44jAghdpW58FKgfqPdotA=="
+      "inviterId":"a99FNTpjt1jCNsakdDrRSw=="
     }), async (err, resp, data) => {
       try {
         if (err) {
