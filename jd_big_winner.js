bc4c2bcdf9023cb9ae0a1ad77c84fe93f53a8f2d
linmudaye <52953782+linmudaye@users.noreply.github.com>
2021-11-10 12:59:20 +0800

    Update jd_big_winner.js

--- jd_big_winner.js
+++ jd_big_winner.js
@@ -9,7 +9,7 @@
 20 * * * * jd_big_winner.js, tag=省钱大赢家之翻翻乐, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
 =================================Loon===================================
 [Script]
-cron "20 * * * *" script-path=jd_big_winner.js,tag=省钱大赢家之翻翻乐
+20,40 * * * * jd_big_winner.js
 ===================================Surge================================
 省钱大赢家之翻翻乐 = type=cron,cronexp="20 * * * *",wake-system=1,timeout=3600,script-path=jd_big_winner.js
 ====================================小火箭=============================
